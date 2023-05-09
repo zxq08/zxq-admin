77927287bbe0a188d693df1f62def34db80f3f8b
zxq <461890276@qq.com>
2023-05-09 22:54:50 +0800

    feat: add useApi

---
+++ src/service/useApi.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import { useState, useEffect, useReducer, Reducer } from 'react'
+import axios from 'axios'
+
+const dataFetchReducer: Reducer<any, any> = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      }
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      }
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      }
+    default:
+      throw new Error()
+  }
+}
+
+const useApi = function (initUrl: string, initData: any) {
+  const [url, setUrl] = useState(initUrl)
+  const [state, dispatch] = useReducer<Reducer<any, any>>(dataFetchReducer, {
+    data: initData,
+    isLoading: false,
+    isError: false
+  })
+  useEffect(() => {
+    let didCancel = false
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' })
+      try {
+        const result = await axios(url)
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILURE' })
+      }
+    }
+    fetchData()
+    return () => {
+      didCancel = true
+    }
+  }, [url])
+  return [state, setUrl]
+}
+export default useApi
